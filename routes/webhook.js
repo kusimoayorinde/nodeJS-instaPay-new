0b002d9e68eb3368f512508cd7b0d1520e0c966e
instaPay <kusimoayorinde@gmail.com>
2025-06-14 16:03:03 +0000

    second commit 14.06.2025

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const crypto = require('crypto');
+const user = require('../models/user');
 
 // Creating express Router
 const router=express.Router()
@@ -24,15 +25,49 @@ const secret = "sk_test_2807786a573f55bd6ba23d17534b28900cfd32c8";
 // Using Express
 router.post("/webhook", (req, res) => {
     console.log("Webhook received");
+
     //validate event
     const hash = crypto.createHmac('sha512', secret).update(JSON.stringify(req.body)).digest('hex');
     
     if (hash == req.headers['x-paystack-signature']) {
         // Retrieve the request's body
         const event = req.body;
-        // Do something with event  
-        console.log(event)
+        
+        //console.log(req.body.event);
+        //console.log(req.body.data.status)
+        //console.log(req.body.data.customer.email)
+
         res.sendStatus(200);
+
+        if (event.event == 'charge.success' && event.data.status == 'success') {
+            // Handle the successful payment here
+            console.log("Payment successful for event:", event.data.reference);
+            // You can update your database or perform other actions here
+            user.findOne({email: event.data.customer.email}).then(user => {
+                if (user) {
+                    // Update user payment status or perform any other action
+                    user.walletBalance += event.data.amount / 100; // Assuming amount is in kobo
+                    user.save().then(() => {
+                        console.log("User wallet updated successfully for email:", event.data.customer.email);
+                        //res.sendStatus(200); // Respond with success
+                        //Details should be logged to success file for monitoring
+                    }).catch(err => {
+                        console.error("Error updating user wallet:", err);
+                        //res.sendStatus(500); // Respond with error
+                        // Details should be logged to error file for further analysis
+                    });
+                } else {
+                    console.log("User not found for email:", event.data.customer.email);
+                    //Details should be logged to error file for further analysis
+                }
+            }).catch(err => {
+                console.error("Error finding user:", err);
+            });
+
+        } else {
+            // Handle other statuses or errors
+            console.log("Payment failed or pending for event:", event);
+        }
 
     }else{
         console.log("Invalid signature");
